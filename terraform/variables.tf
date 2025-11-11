190d4c92a266918191e22dc2ba5a1aad1ef53230
boommb92-svg <boommb92@gmail.com>
2025-11-12 04:11:02 +0530

    Update variables.tf yes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,34 +1,81 @@
-variable "project_id" { type = string, description = "GCP project ID" }
-variable "region" { type = string, default = "us-central1" }
-variable "zone" { type = string, default = "us-central1-a" }
+variable "project_id" {
+  type        = string
+  description = "GCP project ID"
+}
 
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
 
-# Network
-variable "vpc_name" { type = string, default = "ecom-vpc" }
-variable "app_subnet_cidr" { type = string, default = "10.20.1.0/24" }
-variable "db_subnet_cidr" { type = string, default = "10.20.2.0/24" }
+variable "zone" {
+  type    = string
+  default = "us-central1-a"
+}
 
+variable "vpc_name" {
+  type    = string
+  default = "ecom-vpc"
+}
 
-# App VM
-variable "app_machine_type" { type = string, default = "e2-standard-2" }
-variable "app_image" { type = string, default = "projects/debian-cloud/global/images/family/debian-12" }
+variable "app_subnet_cidr" {
+  type    = string
+  default = "10.20.1.0/24"
+}
 
+variable "db_subnet_cidr" {
+  type    = string
+  default = "10.20.2.0/24"
+}
 
-# Jenkins VM
-variable "jenkins_machine_type" { type = string, default = "e2-standard-2" }
-variable "jenkins_allow_cidr" { type = string, default = null, description = "YourIP/32 allowed to access Jenkins (8080)" }
+variable "app_machine_type" {
+  type    = string
+  default = "e2-standard-2"
+}
 
+variable "app_image" {
+  type    = string
+  default = "projects/debian-cloud/global/images/family/debian-12"
+}
 
-# Cloud SQL
-variable "db_tier" { type = string, default = "db-f1-micro" }
-variable "db_version" { type = string, default = "MYSQL_8_0" }
-variable "db_name" { type = string, default = "ecom" }
-variable "db_user" { type = string, default = "ecomapp" }
+variable "jenkins_machine_type" {
+  type    = string
+  default = "e2-standard-2"
+}
 
+variable "jenkins_allow_cidr" {
+  type        = string
+  default     = null
+  description = "YourIP/32 allowed to access Jenkins (8080)"
+}
 
-# SSH
-variable "ssh_source_cidr" { type = string, default = null, description = "YourIP/32 allowed for SSH" }
+variable "db_tier" {
+  type    = string
+  default = "db-f1-micro"
+}
 
+variable "db_version" {
+  type    = string
+  default = "MYSQL_8_0"
+}
 
-# Repo (used by startup to show app)
-variable "app_port" { type = number, default = 80 }
+variable "db_name" {
+  type    = string
+  default = "ecom"
+}
+
+variable "db_user" {
+  type    = string
+  default = "ecomapp"
+}
+
+variable "ssh_source_cidr" {
+  type        = string
+  default     = null
+  description = "YourIP/32 allowed for SSH"
+}
+
+variable "app_port" {
+  type    = number
+  default = 80
+}
