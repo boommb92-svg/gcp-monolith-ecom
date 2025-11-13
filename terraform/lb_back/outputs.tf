abd2e4b7e511b0fd3f62eb9cabfc84ac78a18d62
boommb92-svg <boommb92@gmail.com>
2025-11-13 17:22:20 +0530

    Update outputs.tf

--- terraform/lb_back/outputs.tf
+++ terraform/lb_back/outputs.tf
@@ -1,14 +1,12 @@
 output "lb_ip" {
-  description = "Load Balancer External IP"
+  description = "External Load Balancer IP"
   value       = google_compute_global_forwarding_rule.http_fr.ip_address
 }
 
 output "backend_service" {
-  description = "Backend service self_link"
-  value       = google_compute_backend_service.backend.self_link
+  value = google_compute_backend_service.backend.self_link
 }
 
 output "instance_group" {
-  description = "Instance group self_link"
-  value       = google_compute_instance_group.uig.self_link
+  value = google_compute_instance_group.uig.self_link
 }
