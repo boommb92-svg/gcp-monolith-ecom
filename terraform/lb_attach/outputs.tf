7e1224ff256ef2192c743f09dd4ec7d7c13b0f1f
boommb92-svg <boommb92@gmail.com>
2025-11-13 17:10:45 +0530

    Update outputs.tf
    xxx

--- terraform/lb_attach/outputs.tf
+++ terraform/lb_attach/outputs.tf
@@ -1,11 +1,15 @@
 output "lb_ip" {
-  description = "Public IP of the Load Balancer (may be empty until provisioning finishes)"
-  value = length(google_compute_global_forwarding_rule.lb_fr_https) > 0 ? google_compute_global_forwarding_rule.lb_fr_https[0].ip_address : google_compute_global_forwarding_rule.lb_fr_http[0].ip_address
+  description = "Public IP of the Load Balancer"
+  value = length(google_compute_global_forwarding_rule.lb_fr_https) > 0 ?
+    google_compute_global_forwarding_rule.lb_fr_https[0].ip_address :
+    google_compute_global_forwarding_rule.lb_fr_http[0].ip_address
 }
 
 output "lb_forwarding_rule" {
-  description = "Forwarding rule self_link (HTTPS or HTTP)"
-  value = length(google_compute_global_forwarding_rule.lb_fr_https) > 0 ? google_compute_global_forwarding_rule.lb_fr_https[0].self_link : google_compute_global_forwarding_rule.lb_fr_http[0].self_link
+  description = "Forwarding rule self_link"
+  value = length(google_compute_global_forwarding_rule.lb_fr_https) > 0 ?
+    google_compute_global_forwarding_rule.lb_fr_https[0].self_link :
+    google_compute_global_forwarding_rule.lb_fr_http[0].self_link
 }
 
 output "backend_service" {
@@ -13,7 +17,10 @@ output "backend_service" {
   value       = google_compute_backend_service.lb_backend.self_link
 }
 
-output "managed_cert_status" {
-  description = "Managed certificate provisioning status (if applicable)"
-  value = length(google_compute_managed_ssl_certificate.managed_cert) > 0 ? google_compute_managed_ssl_certificate.managed_cert[0].managed[0].status : ""
+# NOTE: No status attribute exists in your provider version.
+# We output the cert NAME instead (safe & supported everywhere)
+output "managed_cert_name" {
+  description = "Managed SSL Certificate name (if created)"
+  value = length(google_compute_managed_ssl_certificate.managed_cert) > 0 ?
+    google_compute_managed_ssl_certificate.managed_cert[0].name : ""
 }
