230994be63066ddf53a0bd278af6825230888297
boommb92-svg <boommb92@gmail.com>
2025-11-13 17:21:47 +0530

    Update variables.tf

--- terraform/lb_back/variables.tf
+++ terraform/lb_back/variables.tf
@@ -6,34 +6,29 @@ variable "project_id" {
 variable "region" {
   type        = string
   default     = "us-central1"
-  description = "Region of your backend VM"
 }
 
 variable "vm_name" {
   type        = string
-  description = "Name of the existing VM to attach to LB"
+  description = "Name of the existing VM"
 }
 
 variable "vm_zone" {
   type        = string
   default     = "us-central1-a"
-  description = "Zone of the existing VM"
 }
 
 variable "backend_port" {
-  type        = number
-  default     = 80
-  description = "Port exposed by your application on the VM"
+  type    = number
+  default = 80
 }
 
 variable "network" {
-  type        = string
-  default     = "ecom-vpc"
-  description = "VPC network name"
+  type    = string
+  default = "ecom-vpc"
 }
 
 variable "instance_group_name" {
-  type        = string
-  default     = "ecom-app-group"
-  description = "Name of unmanaged instance group"
+  type    = string
+  default = "ecom-app-group"
 }
